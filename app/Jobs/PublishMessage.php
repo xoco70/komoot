a1fbe6ef8797c3d03ef8cae0fb56538ada4fd06e
Julien Cappiello <contact@kendozone.com>
2018-02-07 22:39:38 +0100

    add import

--- app/Jobs/PublishMessage.php
+++ app/Jobs/PublishMessage.php
@@ -10,6 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 
 class PublishMessage implements ShouldQueue
 {
