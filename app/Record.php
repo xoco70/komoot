6bc231fa3681495ef509704359aa99d9a998e7cc
Julien Cappiello <contact@kendozone.com>
2018-02-09 15:56:44 +0100

    did it again

--- app/Record.php
+++ app/Record.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Record extends Model
 {
     public $timestamps = true;
-    protected $table = 'record';
+    protected $table = 'records';
     protected $guarded = ['id'];
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 }
