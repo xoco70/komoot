41c6c9923edfbe63908033643e1047a028e9aee3
Julien Cappiello <contact@kendozone.com>
2018-02-09 15:36:01 +0100

    add Record info

--- app/Record.php
+++ app/Record.php
@@ -6,4 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Record extends Model
 {
+    public $timestamps = true;
+    protected $table = 'association';
+    protected $guarded = ['id'];
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 }
