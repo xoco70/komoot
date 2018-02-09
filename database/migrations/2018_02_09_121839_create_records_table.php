3d32259a00bc028e1562f5241cb2421e59a7a1d6
Julien Cappiello <contact@kendozone.com>
2018-02-09 15:36:36 +0100

    add Record Migration

---
+++ database/migrations/2018_02_09_121839_create_records_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('name')->index();
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
