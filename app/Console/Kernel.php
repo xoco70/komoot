f82d0a2a880c062c84dc74bf63a5d6254aa81a08
Julien <contact@kendozone.com>
2018-02-11 19:49:35 +0000

    Apply fixes from StyleCI

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Console;
 
 use App\Jobs\SendMailDigest;
 use App\MailDigest;
-use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\Cache;
@@ -23,7 +22,8 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     *
      * @return void
      */
     protected function schedule(Schedule $schedule)
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
         // We put in cache the hourly time of mail digest.
         // This will allow us to send async mails
         $expiresAt = now()->addMinutes(60);
-        Cache::put('schedule_timestamp', now() , $expiresAt);
+        Cache::put('schedule_timestamp', now(), $expiresAt);
 
         $digestsByUser = MailDigest::build();
         foreach ($digestsByUser as $email => $digest) {
@@ -48,7 +48,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__ . '/Commands');
+        $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
     }
